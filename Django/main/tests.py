1a96c7cd18c69a6ccf1b70e0127d2eb3b9550779
Nastenko Peter <source-2306@mail.ru>
2021-07-18 13:42:37 +0300

    lesson3 result

--- Django/main/tests.py
+++ Django/main/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.http import HttpResponse
+
+
+# Create your views here.
+
--- Django/main/views.py
+++ Django/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<button><u><h1>Приверка работы<h1><u><button>"
+                        )
+
+
+def about(request):
+    return HttpResponse("<Страница про нас>")
+
